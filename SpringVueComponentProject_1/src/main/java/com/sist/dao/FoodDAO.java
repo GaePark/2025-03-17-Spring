0ad1cb1a30147c4a77bdbc972a7c0a304c5aa2a0
GaePark <qkrwnsgus01@gmail.com>
2025-04-14 15:52:50 +0900

    20250414

--- SpringVueComponentProject_1/src/main/java/com/sist/dao/FoodDAO.java
+++ SpringVueComponentProject_1/src/main/java/com/sist/dao/FoodDAO.java
@@ -1,5 +1,16 @@
 package com.sist.dao;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import java.util.*;
+import com.sist.vo.*;
+import com.sist.mapper.*;
+@Repository
 public class FoodDAO {
-
+	@Autowired
+	private FoodMapper mapper;
+	public List<FoodVO> foodFindData(Map map)
+	{
+		return mapper.foodFindData(map);
+	}
 }
